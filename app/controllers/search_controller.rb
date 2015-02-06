9409387be6a592cf71831fedbbc4f92f93b9ba02
Ian Gawronski <iangawronski@gmail.com>
2015-02-05 19:39:36 -0500

    fixed some typeos - finished

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,6 +2,7 @@ require 'httparty'
 
 class SearchController < ApplicationController
   def search
+    Etsy.api_key = ETSY_API_TOKEN
     @results = nil
     if params[:q]
       respose = Etsy::Request.get('/listings/active', :inlcude ['Images', 'Shop'], :keywords => params[:q])
