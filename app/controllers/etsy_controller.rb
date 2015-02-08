1f730f3c0d3399f89e29966431bcce9025b88bcf
Ian Gawronski <iangawronski@gmail.com>
2015-02-08 11:04:25 -0500

    fixed some typos, a little more progress

--- app/controllers/etsy_controller.rb
+++ app/controllers/etsy_controller.rb
@@ -1,7 +1,7 @@
 class EtsyController < ApplicationController
   def search
     if params[:q]
-      response = Etsy::Request.get('/listings/active', :inlcudes => ['Images', 'Shop'], :keywords => params[:q])
+      response = Etsy::Request.get('/listings/active', :includes => ['Images', 'Shop'], :keywords => params[:q])
       @results = JSON.parse(response.body)['results']
     end
     render :search
